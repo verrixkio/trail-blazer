345625f47ff23c1890bac01bc2326223a7dc9cf1
verrixkio <verrixkio@gmail.com>
2019-03-31 22:13:13 +0000

    got rails talking via the api ready to add react components

--- trail-blazer-api/db/schema.rb
+++ trail-blazer-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190328184555) do
+ActiveRecord::Schema.define(version: 20190331211958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20190328184555) do
     t.datetime "updated_at", null: false
     t.index ["trails_id"], name: "index_comments_on_trails_id"
     t.index ["users_id"], name: "index_comments_on_users_id"
+  end
+
+  create_table "new_trails", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ratings", force: :cascade do |t|
