60bdfcf0356a36e3c5fb4b402fdfda9cbc5591b7
‘RSpidla’ <rspidla@gmail.com>
2019-04-01 16:38:30 +0000

    Update App.js file

--- trail-blazer-frontend/src/App.js
+++ trail-blazer-frontend/src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './application.css';
+import map from './images/trail-demo-image.jpg';
+import './css/application.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="primary primary--full-width">
+        
+      <header className="primary__top-bar">
+      
+        <a href="/" className="logo">Trail Blazers</a>
+    
+      </header>
+
+      <main className="primary__main">
+
+        <div className="container-fluid">
+          
+          <div className="row">
+
+            <div className="col-sm-8 col-sm-push-4 right">
+            
+              <div className="iframe-rwd">
+                <img src={map} className="App-logo" alt="logo" />
+              </div>
+  
+            </div> 
+              
+            <div className="col-sm-4 col-sm-pull-8 left">
+            
+              <div className="trail">
+
+                <button type="button" className="btn btn-primary cd-btn js-msg-panel-trigger" data-panel="main">Submit Request to Fix Trail</button>
+
+              </div>
+
+            </div>
+            
+          </div>
+
+        </div>
+        
+      </main>
+
+      <footer className="primary__btm-bar">
+
+        <ul className="nav navbar-nav">
+          <li>
+            <a href="#">
+              <i className="fa fa-map"></i>
+              Search Trails
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-map-marker"></i>
+              My Trails
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fa fa-user"></i>
+              My Account
+            </a>
+          </li>
+        </ul>
+
+      </footer>
+
       </div>
     );
   }
