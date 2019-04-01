2db713485204e41d5e50130161f398a93b9d7871
Brayden <braydenh51@gmail.com>
2019-04-01 19:59:46 +0000

    need dependancies from master, this a practise branch for setting up the map

--- trail-blazer-frontend/src/App.js
+++ trail-blazer-frontend/src/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+
+
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+export class MapContainer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
+ 
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyDogOOtZl3ChFa_gEgwpDaadpSKP_EKtmk')
+})(MapContainer)
 
-export default App;
+
