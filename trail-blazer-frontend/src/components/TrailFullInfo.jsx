4a9dba2e318b2faf542254ba9930b0c517e4e832
‘RSpidla’ <rspidla@gmail.com>
2019-04-02 19:45:16 +0000

    Add TrailFullInfo.jsx

---
+++ trail-blazer-frontend/src/components/TrailFullInfo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TrailFullInfo extends Component {
+  render() {
+    return (
+
+        <h2>Trail Information</h2>
+    );
+  }
+}
+export default TrailFullInfo;
