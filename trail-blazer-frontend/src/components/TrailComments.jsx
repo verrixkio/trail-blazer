d4c799d586411d9437271979e2dc77eb30460a8e
‘RSpidla’ <rspidla@gmail.com>
2019-04-02 22:58:12 +0000

    Add src/components/TrailComments.jsx

---
+++ trail-blazer-frontend/src/components/TrailComments.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TrailComments extends Component {
+  render() {
+    return (
+
+        <h2>Trail Comments</h2>
+    );
+  }
+}
+export default TrailComments;
