93f4779d2828d6b8f60e91762cf321c5f5a62eda
Brayden <braydenh51@gmail.com>
2019-04-05 18:13:18 +0000

    adding logic for trail forks api comunication

--- trail-blazer-frontend/src/components/IndividualTrailFull.jsx
+++ trail-blazer-frontend/src/components/IndividualTrailFull.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router'
+import axios from 'axios';
 class IndividualTrailFull extends Component {
+
+componentDidMount() {
+  axios.get('https://www.trailforks.com/api/1/trail?id=88331&api_key=docs')
+  .then(response => {
+      this.setState([{
+          lists: response.data
+      }])
+  })
+  .catch(error => console.log(error))
+}
 render() {
 
   return (
-<h1>{this.props.match.params.id}</h1>
+    <div>
+<h1>{this.props.match.params.id} </h1>
+
+</div>
+
   );
 }
 }
