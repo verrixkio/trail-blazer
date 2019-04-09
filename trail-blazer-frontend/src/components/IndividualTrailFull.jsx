aa41b2f9bbaf3d5f8c893ba879396d9c18bb3ab6
‘RSpidla’ <rspidla@gmail.com>
2019-04-09 22:40:39 +0000

    Update trail components render order in IndividualTrailFull.jsx file

--- trail-blazer-frontend/src/components/IndividualTrailFull.jsx
+++ trail-blazer-frontend/src/components/IndividualTrailFull.jsx
@@ -9,28 +9,25 @@ class IndividualTrailFull extends Component {
 render() {
 // this is how you acces the trail id this.props.match.params.id
   return (
+  <div>
+    <div>     
+      <i class="fa fa-star" aria-hidden="true"></i>
+      <h2>Current Trail Rating</h2>
+      <div><RatingsDisplay trailId={this.props.match.params.id}/></div>
+      
+      <i class="fa fa-star-half-empty" aria-hidden="true"></i>
+      <h2>Add Trail Rating</h2>
+      <div><AddRating trailId={this.props.match.params.id}/></div>
+      
+      <hr className="seperator" />
+    </div>
 
     <div>
-      
-      <div className="lists-container trail__display-ratings">
-        
-        <i class="fa fa-star-half-empty" aria-hidden="true"></i>
-        <h2>Add Trail Rating</h2>
-        <AddRating trailId={this.props.match.params.id}/>
-        
-        <i class="fa fa-star" aria-hidden="true"></i>
-        <h2>Current Trail Rating</h2>
-        <RatingsDisplay trailId={this.props.match.params.id}/>
-        
-      </div>
 
-      <div>
+      <TrailFixRequests trailId={this.props.match.params.id}/>
+      <TrailFixRequestDisplay trail_id={this.props.match.params.id}/>
 
-        <TrailFixRequests trailId={this.props.match.params.id}/>
-        <TrailFixRequestDisplay trail_id={this.props.match.params.id}/>
-
-      </div>
-      
+    </div>
     </div>
   );
 }
