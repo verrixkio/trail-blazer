6dc04fddd0c429c9a2509b7d8c34ab0304c6a243
‘RSpidla’ <rspidla@gmail.com>
2019-04-09 22:42:27 +0000

    Remove markup and classes from component

--- trail-blazer-frontend/src/components/TrailFullInfo.jsx
+++ trail-blazer-frontend/src/components/TrailFullInfo.jsx
@@ -21,17 +21,18 @@ class TrailFullInfo extends Component {
   render() {
     console.log(this.props)
     return (
-      <div className="lists-container trail__full-info">
-      <i class="fa fa-info" aria-hidden="true"></i>
-      <h2>Trail Information</h2>
-      <div className="single-trail" key={this.state.trails.id}>
-        <h3>{this.state.trails.name}</h3>
-        <p>{this.state.trails.description}</p>
-        <p>{this.state.trails.length / 1000}km</p>
-        <p><strong>Difficulty of:</strong> {this.state.trails.difficulty}</p>
-        <p>{this.state.trails.condition}</p>
-      </div>    
+      <div>
         
+        <i class="fa fa-info" aria-hidden="true"></i>
+        <h2>Trail Information</h2>
+        <div className="single-trail" key={this.state.trails.id}>
+          <h3>{this.state.trails.name}</h3>
+          <p>{this.state.trails.description}</p>
+          <p>{this.state.trails.length / 1000}km</p>
+          <p><strong>Difficulty of:</strong> {this.state.trails.difficulty}</p>
+          <p>{this.state.trails.condition}</p>
+        </div>
+
       </div>
     );
 }
