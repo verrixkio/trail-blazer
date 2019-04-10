e8178e229d150ada0549160a493e85f8d2dab372
‘RSpidla’ <rspidla@gmail.com>
2019-04-10 13:20:46 +0000

    Update submit button text

--- trail-blazer-frontend/src/components/FixPopUp.jsx
+++ trail-blazer-frontend/src/components/FixPopUp.jsx
@@ -29,17 +29,19 @@ class FixPopUp extends Component {
     return (
       
       <div className="map-bubble">
-        <button onClick={this.props.closePopup}>close me</button>
         <h3>Selected cause:</h3>  
-        <h3>{this.props.solution.description_to_fix}</h3>
+        <p>{this.props.solution.description_to_fix}</p>
         <form onSubmit={this.addDonation}>
-                <div className="form-group">
-                    <label htmlFor="name">Donation</label>
-                    <input className="regular-form" id="name" name="name" type="text" placeholder="name"></input>
-                    <input className="regular-form" id="amount" name="amount" type="integer" placeholder="amount"></input>
-                </div>
-                    <button className="btn btn-block" type="submit">Submit</button>
-            </form>
+          <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input className="regular-form" id="name" name="name" type="text" placeholder="name"></input>
+              <label htmlFor="amount">Amount:</label>
+              <input className="regular-form" id="amount" name="amount" type="integer" placeholder="amount"></input>
+              <label htmlFor="credit-card">Credit Card:</label>
+              <input className="regular-form" id="credit-card" name="credit-card" type="integer" placeholder="credit-card"></input>
+          </div>
+          <button className="btn btn-block" type="submit">Submit Donation</button>
+        </form>
 
       </div>
 
